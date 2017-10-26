db6ec131989cdb5b123f6721dd1d4f7fb3421311
jennyf19 <ferriesj@gmail.com>
2017-10-26 10:22:37 -0700

    commit

---
+++ DisplayBits/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayBits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowBits b = new ShowBits(8);
+            ShowBits c = new ShowBits(32);
+            ShowBits d = new ShowBits(32);
+
+            Console.WriteLine("123 in binary: ");
+            b.Show(123);
+
+            Console.WriteLine("\n87987 in binary: ");
+            c.Show(87987);
+
+            Console.WriteLine("\n237658768 in binary: ");
+            d.Show(237658768);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DisplayBits/ShowBits.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayBits
+{
+    public class ShowBits
+    {
+        int numBits;
+
+        public ShowBits(int n)
+        {
+            numBits = n;
+        }
+
+        public void Show(long value)
+        {
+            long mask = 1;
+
+            // left-shift a 1 into the proper position
+            mask <<= numBits - 1;
+
+            int spacer = 0;
+
+            for(; mask != 0; mask >>=1)
+            {
+                if((value & mask) != 0)
+                {
+                    Console.Write("1");
+                }
+                else
+                {
+                    Console.Write("0");
+                }
+                spacer++;
+                if((spacer%8)==0)
+                {
+                    Console.Write(" ");
+                    spacer = 0;
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
